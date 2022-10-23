40cef54f7b68c7ca22512d57b233e860a2b243c3
Kristen von Gerichten <kristenvongerichten@Kristens-Air.lan>
2022-10-23 16:08:25 -0400

    styled project

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+console.log("Hi!")
+
+let header = document.querySelector("#DurhamNC")
+console.log(header)
+
+let image = document.querySelector("img")
+console.log(img)
+
+let mainHeader = document.querySelector("h1")
+console.log(mainHeader)
+
